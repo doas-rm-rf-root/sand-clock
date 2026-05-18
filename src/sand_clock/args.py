cea7aaf422ff68460d4718286667761b52b49566
doas-rm-rf-root <aslanian.ivan@knu.ua>
2026-05-18 22:52:44 +0300

    Update args.py, args clarification

--- src/sand_clock/args.py
+++ src/sand_clock/args.py
@@ -2,14 +2,14 @@ import argparse
 
 def parse():
 
-    parser = argparse.ArgumentParser(description="sand clock")
+    parser = argparse.ArgumentParser(description="A small hourglass app based on cellular automate and taichi. CPU renderer currently")
 
     parser.add_argument(
         'number',
         type=int,
         nargs='?',
         default=15,
-        help="число для обработки"
+        help="Number of timed minuted"
     )
 
     args = parser.parse_args()
